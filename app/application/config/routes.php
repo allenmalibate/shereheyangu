7c95bad820a872f8ae0d275f5c16be1d6f0e048f
Joseph Chingalo <profschingalo@gmail.com>
2015-10-02 09:36:19 +0300

    initate admin page and enable urls from routes

--- app/application/config/routes.php
+++ app/application/config/routes.php
@@ -50,5 +50,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
+
+//routes for administrations
+$route['admin'] = "admin/adminLoginForm";
+$route['admin-home'] = "admin/adminHome";
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
