6a39ca76b32a6de2567e912a430d965205f71236
Joseph Chingalo <profschingalo@gmail.com>
2015-11-15 04:56:32 +0300

    validation rules during login into system

--- app/application/controllers/User.php
+++ app/application/controllers/User.php
@@ -13,16 +13,48 @@ class User extends CI_Controller{
      */
     function logIn(){
 
-        $this->User_model->login();
-        print_r('login into system');
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('password','password','trim|required');
+        $this->form_validation->set_rules('email','e-mail','trim|valid_email|required');
+
+        if($this->form_validation->run() === FALSE){
+
+            $this->memberForms();
+        }else{
+
+            print_r('login into system');
+        }
+
     }
 
     /*
      * function for register new member into system
      */
     function createAccount(){
+        /*
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('','','trim|required');
+
+        if($this->form_validation->run() === FALSE){
+
+            $this->memberForms();
+        }else{
+
+            print_r('login into system');
+        }
+        */
 
         print_r('create account');
+    }
+
+
+    /*
+     * function to login form
+     */
+    function memberForms(){
+        $this->load->view("home/includes/top_base");
+        $this->load->view("home/page/members");
+        $this->load->view("home/includes/bottom_base");
     }
 
 
--- app/application/views/home/page/members.php
+++ app/application/views/home/page/members.php
@@ -18,13 +18,32 @@
                                     <form class="form-horizontal" style="padding-top:33px;" action ="<?php echo site_url('login'); ?>" method = "POST" >
 
                                         <div class="row">
-                                            <div class="col-lg-6">
+                                            <?php if(form_error('email')): ?>
+                                                <div class="alert alert-warning" role="alert">
+                                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                    <?php echo form_error('email'); ?>
+                                                </div>
+                                            <?php endif;?>
+                                            <?php if(form_error('password')): ?>
+                                                <div class="alert alert-warning" role="alert">
+                                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                    <?php echo form_error('password'); ?>
+                                                </div>
+                                            <?php endif;?>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-lg-6 col-sm-12 col-md-6 col-xs-12">
                                                 <div class="form-group">
-                                                    <input type="text" name="email" id="email" class="form-control input-lg" placeholder="Email" tabindex="1">
+                                                    <input type="text" value = "<?php echo set_value('email');?>"  name="email" id="email" class="form-control input-lg" placeholder="Email" tabindex="1">
                                                 </div>
                                             </div>
-                                            <div class="col-lg-6">
-                                                <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="1">
+                                            <div class="col-lg-6 col-sm-12 col-md-6 col-xs-12">
+                                                <input type="password"  name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="1">
                                             </div>
                                         </div>
 
@@ -39,27 +58,36 @@
                                 </div>
                                 <div class="tab-pane fade" id="create">
                                     <center><h2 style="color:rgba(191,72,74,1.00);"> Create your account, It's Free...! </h2></center>
-                                    <form id="tab" style="padding-top:20px;">
+                                    <form id="tab" style="padding-top:20px;" action="<?php echo site_url('create-account'); ?>" method="POST" >
 
                                         <div class="row">
-                                            <div class="col-lg-6">
+                                            <div class="col-lg-6 col-sm-12 col-md-6 col-xs-12">
                                                 <div class="form-group">
                                                     <input type="text" name="firstname" id="firstname" class="form-control input-lg" placeholder="First Name" tabindex="1">
                                                 </div>
                                             </div>
-                                            <div class="col-lg-6">
+                                            <div class="col-lg-6 col-sm-12 col-md-6 col-xs-12">
                                                 <input type="text" name="lastname" id="lastname" class="form-control input-lg" placeholder="Last Name" tabindex="1">
                                             </div>
                                         </div>
-                                        <div class="form-group">
-                                            <input type="text" name="email" id="email" class="form-control input-lg" placeholder="Your Email" tabindex="1">
+                                        <div class="row" style="margin-top: 2%;">
+                                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                                                <div class="form-group">
+                                                    <input type="text" name="email" id="email" class="form-control input-lg" placeholder="Your Email" tabindex="1">
+                                                </div>
+
+                                                <div class="form-group">
+                                                    <input type="text" name="email_confirm" id="email-confirm" class="form-control input-lg" placeholder="Confirm your email" tabindex="1">
+                                                </div>
+                                            </div>
                                         </div>
 
-                                        <div class="form-group">
-                                            <input type="text" name="email_confirm" id="email-confirm" class="form-control input-lg" placeholder="Confirm your email" tabindex="1">
-                                        </div>
 
-                                        <center><a href="firstForm.php" class="btn btn-xl btn-info"><i class="fa fa-plus"></i> CREATE ACCOUNT</a> </center>
+                                        <center>
+                                            <button class="btn btn-xl btn-info" type="submit">
+                                                <i class="fa fa-plus"></i> CREATE ACCOUNT
+                                            </button>
+                                        </center>
 
                                     </form>
                                 </div>
