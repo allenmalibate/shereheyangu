205dcc1d490485a01d637dfac612ced19778ac6b
Joseph Chingalo <profschingalo@gmail.com>
2015-11-16 14:20:46 +0300

    starting admin interface

--- app/application/views/admin/adminHome.php
+++ app/application/views/admin/adminHome.php
@@ -1,1 +1,19 @@
-admin home
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+            <div class="panel panel-primary">
+                <div class="panel-body">
+                    <ul class="nav nav-pills nav-stacked">
+                        <li role="presentation" ><a href="#">Home</a></li>
+                        <li role="presentation"><a href="#">Profile</a></li>
+                        <li role="presentation"><a href="#">Messages</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xs-12 col-sm-8 col-md-9 col-lg-9">
+
+        </div>
+    </div>
+</div>
--- app/application/views/admin/includes/top_base.php
+++ app/application/views/admin/includes/top_base.php
@@ -23,3 +23,33 @@
     <![endif]-->
 </head>
 <body>
+<div class = "">
+    <nav class="navbar navbar-inverse" style="border-radius: 0px;">
+        <div class="container-fluid" style="padding-bottom: 3%;">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="<?php echo site_url('admin-home');?>">
+                    <img alt="Brand" src="<?php echo base_url();?>img/brand.png">
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right" style="margin-right: 3px;">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">My Profile <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">My Profile</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="<?php echo site_url('log-out'); ?>">log out</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
